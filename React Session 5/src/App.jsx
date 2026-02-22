73a4707eadcf29a8d69932e641c8725da4017bee
KedarG28 <2024ci19f@sigce.edu.in>
2026-02-22 13:21:44 +0530

    Counter practice & Media Query Scss

---
+++ React Session 5/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Counter from './assets/components/counter'
+
+function App() {
+
+
+  return (
+    <>
+      <Counter/>
+      <div className="page1">
+        <p>Hello</p>
+      </div>
+    </>
+  )
+}
+
+export default App
