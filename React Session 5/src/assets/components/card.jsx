618d87f8af13079c5d10d5d1821f5c554a08b445
KedarG28 <2024ci19f@sigce.edu.in>
2026-02-22 16:23:16 +0530

    userdata is important, useEffect is used to get the whole data once so that the other things do not re render.

--- React Session 5/src/assets/components/card.jsx
+++ React Session 5/src/assets/components/card.jsx
@@ -1,41 +1,39 @@
-import React from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
-import  Person from '../person.json'
-import axios from 'axios';
+import { useState } from "react";
+import { useEffect } from "react";
+import Person from "../person.json";
+import axios from "axios";
 
 const Card = () => {
+  const [person, setPerson] = useState([]);
+  console.log("123", person);
 
-const [person,setPerson] = useState([]);
-console.log('123',person)
-
-useEffect(()=>{
-    const hello = async ()=>{
-        try {
-            const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
-            console.log(response.data)
-            setPerson(response.data)
-        } catch (error) {
-            // console.log('error')
-        }
-    }
+  useEffect(() => {
+    const hello = async () => {
+      try {
+        const userdata = await axios.get(
+          "https://jsonplaceholder.typicode.com/todos/1",
+        );
+        console.log(userdata.data);
+        setPerson(userdata.data);
+      } catch (error) {
+        // console.log('error')
+      }
+    };
     hello();
-},[])
-
+  }, []);
 
   return (
     <>
-    <p>
-        Person : {Person.map((e)=>{
-            return e.name
+      <p>
+        Person :{" "}
+        {Person.map((e) => {
+          return e.name;
         })}
-    </p>
+      </p>
 
-    <p>
-        Outside Data : {person.title}
-    </p>
+      <p>Outside Data : {person.title}</p>
     </>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
